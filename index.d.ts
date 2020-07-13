436e962921dc1658b047d3d6122b107ae7524b48
fittachihz <60156946+fittachihz@users.noreply.github.com>
2020-07-13 15:45:17 +0430

    Create index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,19 @@
+import { Middleware} from 'koa';
+
+declare namespace KoaTooBusy {
+    interface KoaTooBusyOptionsObject {
+        maxLag?: number
+        interval?: number
+        status?: number
+        message?: string
+    }
+
+    type KoaTooBusyOptions = KoaTooBusyOptionsObject
+
+    interface KoaTooBusy {
+        (options: KoaTooBusyOptions): Middleware;
+    }
+}
+
+declare const KoaTooBusy: KoaTooBusy.KoaTooBusy;
+export = KoaTooBusy;
